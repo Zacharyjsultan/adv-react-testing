6df0a66569a22060d4748b037819410558fa6c1c
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2023-01-17 21:08:06 -0800

    shoppinglistofrm2/3

--- src/components/ShoppingList/ShoppingList.test.jsx
+++ src/components/ShoppingList/ShoppingList.test.jsx
@@ -1,33 +1,15 @@
-// eslint-disable-next-line no-unused-vars
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import ShoppingList from './ShoppingList.jsx';
 
 describe('Shopping List', () => {
   test('render shopping list name', () => {
-    const shoppingList = {
+    const choppingList = {
       id: 1,
       name: 'Eggos List',
-      shoppingItems: [{ id: 1, name: 'Eggos', quantity: 200 }],
+      shoppingItems: [{ id: 1, name: 'Eggos', quantity: 2000 }],
     };
-    render(<ShoppingList shoppingList={shoppingList} />);
-    const listName = screen.getByText(shoppingList.name);
-    expect(listName).toBeInTheDocument();
+    render(<ShoppingList shoppingList={choppingList} />);
+    const list = screen.getByText(choppingList.name);
+    expect(list).toBeInTheDocument();
   });
-  //   test('create new shopping item', () => {
-  //     const choppingList = {
-  //       id: 1,
-  //       name: 'Eggos List',
-  //       shoppingItems: [{ id: 1, name: 'Eggos', quantity: 2000 }],
-  //     };
-  //     const onCreateShoppingItem = jest.fn();
-  //     render(
-  //       <ShoppingList
-  //         shoppingList={choppingList}
-  //         onCreateShoppingItem={onCreateShoppingItem}
-  //       />
-  //     );
-  //     const submitButton = screen.getByText('submit');
-  //     fireEvent.click(submitButton);
-  //     expect(onCreateShoppingItem).toHaveBeenCalled();
-  //   });
 });
